99713dbf9152f0b19dfe5f7f206b205a97813ffe
shmoti <kyrica777vitya@gmail.com>
2025-06-08 18:33:15 +0300

    Update OXION.lua

--- OXION.lua
+++ OXION.lua
@@ -8,16 +8,6 @@ end
 
 local module = require(11495711008)
 
-local minID = 11478254
-local maxID = 45678902
-local randomID = math.random(minID, maxID)
-
-for i = 1, 50 do
-	wait(0.01)
-	warn("Failed to load animation: rbxassetid://" .. randomID)
-end
-
-
 local bot = module.DeclareBot("7876117719:AAGEN_Jm0G4W_w1td5xMVsfUK6lTHON5kPo")
 
 local placeID = game.PlaceId
